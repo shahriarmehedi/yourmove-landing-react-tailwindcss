9686b6a3640236b101dd332199a803192bcd485d
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-11-27 00:30:13 +0600

    button changed

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -29,7 +29,7 @@ const ContactForm = () => {
                             </div>
                         </div>
                         <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Send</button>
+                            <button className='bg-sky-500 flex mx-auto mt-5 text-white px-10 py-[12px] rounded-md text-sm font-semibold hover:bg-sky-600 transition duration-200'>Send </button>
                         </div>
 
 
